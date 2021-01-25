4f010965509262662d7ac58d96feca1be74f091a
Alex Lancaster <aaa633@ucsb.edu>
2021-01-25 08:34:40 -0800

    if this isn't right i will cry

--- marblesTree.cpp
+++ marblesTree.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 
 // "I Wrote This" by Alex Lancaster (6706998)
 
@@ -30,74 +29,50 @@ void CountAndSetTargets(Box& b, Box* boxList) {
     }
 }
 
-// move the cheapest marbles "up" to the parent of this node.
-// the queue order always finds the lower cost marbles first.
-// O(n)
-void Leech(Box& b, Box* boxList, int& moveCounter) {
-    int marblesToErase = b.currentSubTotal - b.targetSubTotal;
-    std::queue<Box*> boxQueue;
-    std::queue<int> costQueue;
-    boxQueue.push(&b);
-    costQueue.push(1);
+//O(n)
+void MoveUp(Box& curr, Box& parent, Box* boxList, int& moveCounter) {
+    for (int i = 0; i < curr.children.size(); ++i) {
+        Box& child = boxList[curr.children[i]];
+	MoveUp(child, curr, boxList, moveCounter);
+    }
 
-    while (!boxQueue.empty()) {
-        Box& c = (*(boxQueue.front())); boxQueue.pop();
-	int cost = costQueue.front(); costQueue.pop();
-
-        if (c.marbles > c.targetSubTotal) {
-	    int toRemove = c.marbles - c.targetSubTotal;
-            c.marbles -= toRemove;
-	    moveCounter += toRemove*cost;
-	    marblesToErase -= toRemove;
-	    if (marblesToErase < 0) {
-	        c.marbles += -marblesToErase;
-		moveCounter -= -marblesToErase*cost;
-		marblesToErase = 0;
-	    }
-	    if (marblesToErase == 0) {
-	        break;
-	    }
-        }
-
-        for (int i = 0; i < c.children.size(); ++i) {
-            boxQueue.push(&boxList[c.children[i]]);
-            costQueue.push(cost + 1);
-        }
+    int excess = curr.currentSubTotal - curr.targetSubTotal;
+    if (excess > 0) {
+        curr.marbles -= excess;
+	parent.marbles += excess;
+	moveCounter += excess;
     }
 }
 
-// O(n^2). Maybe there is a way to do better.
-void Redistribute(Box& b, Box* boxList, int& moveCounter) {
-    std::vector<int> underfunded;
-    std::vector<int> overfunded;
+//O(n)
+void MoveDown(Box& curr, Box* boxList, int& moveCounter) {
+    for (int i = 0; i < curr.children.size(); ++i) {
+        Box& child = boxList[curr.children[i]];
+	int wanted = child.targetSubTotal - child.currentSubTotal;
+	if (wanted > 0) {
+	    curr.marbles -= wanted;
+	    child.marbles += wanted;
+	    moveCounter += wanted;
+	}
+
+	MoveDown(child, boxList, moveCounter);
+    }
+
+}
+
+//O(n)!!!
+void Redistribute(Box* boxList, int& moveCounter) {
+    Box& b = boxList[0];
 
     //find out which subtrees have too few or too many marbles.
     for (int i = 0; i < b.children.size(); ++i) {
-        Box& c = boxList[b.children[i]];
-        if (c.targetSubTotal < c.currentSubTotal) { overfunded.push_back(i); }
-        else if (c.targetSubTotal > c.currentSubTotal) { underfunded.push_back(i); }
+        Box& child = boxList[b.children[i]];
+        MoveUp(child, b, boxList, moveCounter);
     }
 
-    // somehow, find the cheapest marbles and "bring them to this root."
-    for (int i = 0; i < overfunded.size(); ++i) {
-        Leech(boxList[b.children[overfunded[i]]], boxList, moveCounter);
-    }
+    CountAndSetTargets(b, boxList);
 
-    // "place them into" the trees that are below their target.
-    for (int i = 0; i < underfunded.size(); ++i) {
-        Box& c = boxList[b.children[underfunded[i]]];
-	int add = c.targetSubTotal - c.currentSubTotal;
-	moveCounter += add;
-        c.marbles += add;
-	c.currentSubTotal = c.targetSubTotal;
-    }
-
-    // b.marbles = 1; 
-
-    for (int i = 0; i < b.children.size(); ++i) {
-	CountAndSetTargets(boxList[b.children[i]], boxList);
-        Redistribute(boxList[b.children[i]], boxList, moveCounter);
-    }
+    MoveDown(b, boxList, moveCounter);
 }
 
 bool Solve() {
@@ -120,20 +95,8 @@ bool Solve() {
 
     CountAndSetTargets(boxList[0], boxList);
 
-    /*
-    std::cout << "----------TEST A---------" << std::endl;
-
-    for (int i = 0; i < boxCount; ++i) {
-        std::cout << "Box #" << i << std::endl;
-	std::cout << "\t Should have " << boxList[i].targetSubTotal << " marbles in its subtree;" << std::endl;
-	std::cout << "\t Right now there are " << boxList[i].currentSubTotal << "." << std::endl;
-    }
-
-    std::cout << "----------------------" << std::endl;
-    */
-
     int moveCounter = 0;
-    Redistribute(boxList[0], boxList, moveCounter);
+    Redistribute(boxList, moveCounter);
 
     std::cout << moveCounter << std::endl;
 
@@ -143,8 +106,8 @@ bool Solve() {
 int main(int argc, char** argv) {
     
     //std::ifstream in("test.txt");
-    //std::streambuf *cinbuf = std::cin.rdbuf(); //save old buf
-    //std::cin.rdbuf(in.rdbuf()); //redirect std::cin to in.txt!
+    //std::streambuf* cinbuf = std::cin.rdbuf();
+    //std::cin.rdbuf(in.rdbuf());
 
     for (int i = 0; i < 10; ++i) {
         bool end = Solve();
